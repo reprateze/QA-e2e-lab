bac3b2c318c7f3e4811ea54babdf4a64a76e3a30
Renan Prateze <145148823+reprateze@users.noreply.github.com>
2026-09-16 08:56:51 -0300

    Modify pedido_concluido_visivel to include timeout
    Added a timeout parameter to the 'pedido_concluido_visivel' method to allow customization of the wait duration.

--- pages/payment_page.py
+++ pages/payment_page.py
@@ -28,5 +28,5 @@ class  PaymentPage(BasePage):
         except PlaywrightTimeoutError:
             pass  
 
-    def pedido_concluido_visivel(self) -> bool:
-        return self.page.wait_for_locator(self.PEDIDO_CONCLUIDO).is_visible()
+    def pedido_concluido_visivel(self, timeout: int = 5000) -> bool:
+        return self.page.wait_for_locator(self.PEDIDO_CONCLUIDO, timeout=timeout).is_visible()

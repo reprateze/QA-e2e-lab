44910d727786ac10efb548635f804f644570629d
Renan Prateze <145148823+reprateze@users.noreply.github.com>
2026-09-16 19:03:48 -0300

    Fix variable name from 'quant' to 'quan'

--- pages/base_page.py
+++ pages/base_page.py
@@ -46,10 +46,10 @@ class BasePage:
             total_texto = linha.locator(self.CART_TOTAL).inner_text()
 
             preco = self._extrair_valor(preco_texto)
-            quant = int(quantidade_texto)
+            quan = int(quantidade_texto)
             total_exibido = self._extrair_valor(total_texto)
 
-            if preco * quant != total_exibido:
+            if preco * quan != total_exibido:
                 return False
 
         return True
